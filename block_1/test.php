4590e4d297c307b6826cd313dd8856cb04563174
Sentens <54134600+Sentens@users.noreply.github.com>
2019-08-25 21:44:51 +0300

    Revert "loading"
    This reverts commit 6746dddf5d35f5bb7e058c5c380f95ccc4faa169.

--- block_1/test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$week = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
-
-    $date = date_create(date('Y-m-d'));
-    date_modify($date, '-100 days');
-    echo $week[date_format($date, 'w')];
- ?>
