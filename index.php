7d4b7973ec9811f72e07ca087596ef60b1fdf9f5
Sentens <54134600+Sentens@users.noreply.github.com>
2019-08-25 21:45:19 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Comments</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="css/app.css" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    Project
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="login.html">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="register.html">Register</a>
+                            </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header"><h3>Комментарии</h3></div>
+
+                            <div class="card-body">
+                              <div class="alert alert-success" role="alert">
+                                Комментарий успешно добавлен
+                              </div>
+<?php 
+$comments = [
+    ['user_photo' => 'img/no-user.jpg',
+     'user_name' => 'Валерий К.',
+     'comm_date' => '12/10/2015',
+     'comment' => 'С другой стороны консультация с широким активом играет важную роль в формировании форм развития. С другой стороны дальнейшее развитие различных форм деятельности требуют от нас анализа направлений прогрессивного развития!'
+    ],
+    ['user_photo' => 'img/no-user.jpg',
+     'user_name' => 'Николай Т.',
+     'comm_date' => '13/10/2015',
+     'comment' => 'По своей сути рыбатекст является альтернативой традиционному lorem ipsum, который вызывает у некторых людей недоумение при попытках прочитать рыбу текст..!'
+    ],
+    ['user_photo' => 'img/no-user.jpg',
+     'user_name' => 'Валерия Барда',
+     'comm_date' => '14/10/2015',
+     'comment' => 'Товарищи! укрепление и развитие структуры представляет собой интересный эксперимент проверки позиций, занимаемых участниками в отношении поставленных задач.!'
+    ]
+];
+
+
+
+foreach ($comments as $user) {
+ ?>
+                                <div class="media">
+                                  <img src="<?php echo $user['user_photo']; ?>" class="mr-3" alt="..." width="64" height="64">
+                                  <div class="media-body">
+                                    <h5 class="mt-0"><?php echo $user['user_name']; ?></h5> 
+                                    <span><small><?php echo $user['comm_date']; ?></small></span>
+                                    <p>
+                                        <?php echo $user['comment']; ?>
+                                    </p>
+                                  </div>
+                                </div>
+<?php 
+}
+?>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col-md-12" style="margin-top: 20px;">
+                        <div class="card">
+                            <div class="card-header"><h3>Оставить комментарий</h3></div>
+
+                            <div class="card-body">
+                                <form action="add_comment.php" method="post">
+                                    <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Имя</label>
+                                    <input name="user_name" class="form-control" id="exampleFormControlTextarea1" />
+                                  </div>
+                                  <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Сообщение</label>
+                                    <textarea name="comment" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                  </div>
+                                  <button type="submit" class="btn btn-success">Отправить</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
